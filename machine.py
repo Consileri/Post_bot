4a25e8ab666d2f4d5343c6359864f40a252e07b1
Александр Шахов <47276309+Consileri@users.noreply.github.com>
2020-04-05 17:29:16 +0300

    коррекция под os

--- machine.py
+++ machine.py
@@ -1,6 +1,7 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
+from commander.commander import Commander
 
 
 def main():
@@ -8,19 +9,23 @@ def main():
         token="68b38a0e0b7e909d8535d5508795ae46d9c6175ffd694c28428dd3e63a0738d737667112f465e297887c8")
 
     longpoll = VkBotLongPoll(vk_session, 193785456)
-
+    
+    commander = Commander()
+    
     for event in longpoll.listen():
 
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            print(event)
-            print('Новое сообщение:')
-            print('Для меня от:', event.obj.message['from_id'])
-            print('Текст:', event.obj.message['text'])
-            vk = vk_session.get_api()
-            vk.messages.send(user_id=event.obj.message['from_id'],
-                             message="тест",
-                             random_id=random.randint(0, 2 ** 64))
+        if event.type == VkEventType.MESSAGE_NEW:
 
+            if event.to_me:
 
-if __name__ == '__main__':
-    main()
+                print(f'New message from {event.user_id}', end='')
+
+                bot = VkBot(event.user_id)
+
+                if event.text[0] == "/":
+                    write_msg(event.user_id, commander.do(event.text[1::]))
+                else:
+                    write_msg(event.user_id, bot.new_message(event.text))
+
+                print('Text: ', event.text)
+                print("-------------------")
