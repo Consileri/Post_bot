51b3df98d23ac0dfd749ce3132463a4b99055910
andrey <zhogov.andrey.d@gmail.com>
2020-04-13 11:19:38 +0300

    Добавлена кнопка выбора (почтальон или покупатель) в регистрационной форме

--- forms/register.py
+++ forms/register.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, RadioField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
@@ -10,4 +10,5 @@ class RegisterForm(FlaskForm):
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
     about = TextAreaField("Немного о себе")
+    selection = RadioField('Label', choices=[('value', 'Я почтальон'), ('value_two', 'Я покупатель')])
     submit = SubmitField('Войти')
