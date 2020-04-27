383800e39b546486dba20c718690bc75240b5af9
Александр Шахов <47276309+Consileri@users.noreply.github.com>
2020-04-27 14:38:57 +0300

    В словарь можно добавить имя и активность

--- machine.py
+++ machine.py
@@ -2,45 +2,73 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
+
 dilivered_flag = False
 status = ''
 order_id = ''
-adress = ''
-
-
+sessionStorage = {}
 
 
 def main():
     vk_session = vk_api.VkApi(
         token="68b38a0e0b7e909d8535d5508795ae46d9c6175ffd694c28428dd3e63a0738d737667112f465e297887c8")
-
+    vk = vk_session.get_api()
 
     longpoll = VkBotLongPoll(vk_session, 193785456)
 
     for event in longpoll.listen():
-
         if event.type == VkBotEventType.MESSAGE_NEW:
             print(event)
             print('Новое сообщение:')
             print('Для меня от:', event.obj.message['from_id'])
             print('Текст:', event.obj.message['text'])
-            vk = vk_session.get_api()
-            if event.obj.message['text'] == 'помощь' or event.obj.message['text'] == 'Помощь':
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"На этой странице можно задать вопрос\nhttps://vk.com/hakureireimu\nСписок доступных команд:\nСтатус - просмотреть статус вашего заказа",
-                                 random_id=random.randint(0, 2 ** 64))
-            elif event.obj.message['text'] == 'статус' or event.obj.message['text'] == 'Статус':
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Ваш заказ {status}",
-                                 random_id=random.randint(0, 2 ** 64))
-            elif dilivered_flag:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f'Ваш заказ {order_id} ожидает в пункте выдачи {adress}',
-                                 random_id=random.randint(0, 2 ** 64))
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message='Ничего не понял',
-                                 random_id=random.randint(0, 2 ** 64))
+            handle_dialog(event, vk)
+
+
+
+def handle_dialog(event, vk):
+
+    user_id = event.obj.message['from_id']
+    message = event.obj.message['text']
+    rndm = random.randint(0, 2 ** 64)
+    if user_id in sessionStorage:
+        quests = sessionStorage[user_id]['last_question']
+        if quests == 1:
+            vk.message.send(user_id=user_id,
+                            message=f"Приятно познакомиться, {message}. Кем бы вы хотели быть, почтальоном или"
+                                    f" заказчиком?",
+                            random_id=rndm)
+            sessionStorage[user_id]['name'] = message
+            sessionStorage[user_id]['last_question'] = 2 # 2 - Выбор деятельности
+            return
+        if quests == 2:
+            sessionStorage[user_id]['activity'] = None
+            role = sessionStorage[user_id]['activity']
+            while role is None:
+                if message == 'Почтальон' or 'почтальон':
+                    message = message.lower()
+                    role = message
+                    sessionStorage[user_id]['last_question'] = 3
+                elif message == 'Заказчик' or 'заказчик':
+                    message = message.lower()
+                    role = message
+                    sessionStorage[user_id]['last_question'] = 3
+                else:
+                    vk.messages.send(user_id=user_id,
+                                     message="Вам нужно написать 'почтальон' или 'заказчик'")
+                    continue
+            return
+        if quests == 3:
+
+
+    else:
+        vk.messages.send(user_id=user_id,
+                         message="Вас приветствует Post Bot! Как нам Вас стоит называть?(Напишите только имя)",
+                         random_id=rndm)
+        sessionStorage[user_id] = {
+            'last_question' : 1      # 1 - как вас зовут
+                                   }
+        return
 
 if __name__ == '__main__':
     main()
