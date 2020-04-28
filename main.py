154bf9fc533210d19956299b2eef02fd27bf9435
andrey <zhogov.andrey.d@gmail.com>
2020-04-28 19:23:30 +0300

    убрана кнопка с подтверждением заказа

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_login import LoginManager, login_user, login_required, logout_user, \
     current_user
-from flask_restful import Api
+
 from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
 
